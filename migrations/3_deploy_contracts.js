1df8fa7657b3060ffcb6349c7afdc4306bebd3af
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2018-08-01 18:09:04 +0500

    fix possible truffle lag on deploy of token

--- migrations/3_deploy_contracts.js
+++ migrations/3_deploy_contracts.js
@@ -1,11 +1,13 @@
 const Crowdsale = artifacts.require("PreSale");
 const Token = artifacts.require("MNTL");
 
-module.exports = function(deployer) {
+
+module.exports = async function(deployer) {
     const date = new Date();
     const start_at = (new Date(date.getFullYear(), date.getMonth(), 1)).getTime() / 1000;
     const close_at = (new Date(date.getFullYear(), date.getMonth() + 1, 0)).getTime() / 1000;
     const softcap = 1250000;
     const hardcap = 3250000;
-    deployer.deploy(Crowdsale, Token.address, start_at, close_at, softcap, hardcap, web3.eth.accounts[0]);
+    const token = await Token.deployed();
+    deployer.deploy(Crowdsale, token.address, start_at, close_at, softcap, hardcap, web3.eth.accounts[0]);
 };
